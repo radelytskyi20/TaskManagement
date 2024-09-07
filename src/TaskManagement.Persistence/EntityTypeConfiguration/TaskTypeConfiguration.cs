928a0e68b014736d08ec2910c90a629cbb1b78db
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-09-07 19:40:12 +0300

    Entities were cretaed. Added persistence layer with entitiy configurations and ApplicationDbContext

---
+++ src/TaskManagement.Persistence/EntityTypeConfiguration/TaskTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TaskManagement.Persistence.EntityTypeConfiguration
+{
+    public class TaskTypeConfiguration : IEntityTypeConfiguration<Domain.Entities.Task>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.Task> builder)
+        {
+            builder.HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId)
+                .IsRequired(true)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/TaskManagement.Persistence/EntityTypeConfiguration/UserTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Persistence.EntityTypeConfiguration
+{
+    public class UserTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasIndex(user => user.Username).IsUnique();
+            builder.HasIndex(user => user.Email).IsUnique();
+        }
+    }
+}
