b98746585215dfc2736a52a7ff3e9313bedcc1fe
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-09-07 20:38:21 +0300

    UserRepository was created

---
+++ src/TaskManagement.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Interfaces;
+using User = TaskManagement.Domain.Entities.User;
+
+namespace TaskManagement.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UserRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Users.AddAsync(user, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(user => user.Email == email, cancellationToken);
+        }
+
+        public async Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(user => user.Username == username, cancellationToken);
+        }
+    }
+}
